efd08f50ae9bea6718a1b17d6e55413ed73ac1d3
Darya Filippskih <darjushka.fil@yandex.ru>
2023-07-11 15:17:37 +0300

    пр14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,26 +5,44 @@ const handleAuthError = (res, req, next) => {
   next(new UnauthorizedError('Необходима авторизация'));
 };
 
-const extractBearerToken = (header) => header.replace('Bearer ', '');
-
 module.exports.auth = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return handleAuthError(res, req, next);
-  }
-
-  // const token = authorization.replace('Bearer ', '');
-  const token = extractBearerToken(authorization);
-  let payload;
-
   try {
-    payload = jwt.verify(token, 'some-very-incredible-very-important-and-unbelievable-secret-key');
+    const { jwt: token } = req.cookies;
+    if (!token) {
+      handleAuthError();
+    }
+    let payload;
+    try {
+      payload = jwt.verify(
+        token,
+        'some-very-incredible-very-important-and-unbelievable-secret-key',
+      );
+    } catch (err) {
+      return handleAuthError(req, res, next);
+    }
+    req.user = payload;
+    return next();
   } catch (err) {
-    return handleAuthError(res, req, next);
+    return next(err);
   }
-
-  req.user = payload; // записываем пейлоуд в объект запроса
-
-  return next(); // пропускаем запрос дальше
 };
+// const { authorization } = req.headers;
+
+// if (!authorization || !authorization.startsWith('Bearer ')) {
+//   return handleAuthError(res, req, next);
+// }
+
+// const token = authorization.replace('Bearer ', '');
+//   const token = extractBearerToken(authorization);
+//   let payload;
+
+//   try {
+//   payload = jwt.verify(token, 'some-very-incredible-very-important-and-unbelievable-secret-key');
+//   } catch (err) {
+//     return handleAuthError(res, req, next);
+//   }
+
+//   req.user = payload; // записываем пейлоуд в объект запроса
+
+//   return next(); // пропускаем запрос дальше
+// };
