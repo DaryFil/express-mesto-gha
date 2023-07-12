85470824a80b49f1a3ca4635f92ecfa38164775d
Darya Filippskih <darjushka.fil@yandex.ru>
2023-07-12 14:44:55 +0300

    second

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUsers, searchUserById, updateUser, updateAvatar, getUserInfo,
 } = require('../controllers/users');
 
-const { updateUserCelebrate, userIdCelebrate, updateAvatarCelebrate } = require('../validators/users');
+const BadRequest = require('../errors/bad-request');
+
+const validationId = (value) => {
+  if (!mongoose.isValidObjectId(value)) {
+    throw new BadRequest('Переданы некоректные данные при удалении карточки');
+  } else { return value; }
+};
 
 // Получить данные пользователей:
 router.get('/', getUsers);
@@ -13,12 +21,25 @@ router.get('/', getUsers);
 router.get('/me', getUserInfo);
 
 // Обновить профиль:
-router.patch('/me', updateUserCelebrate, updateUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUser);
 
 // Обновить аватар:
-router.patch('/me/avatar', updateAvatarCelebrate, updateAvatar);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(/^((http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/),
+  }),
+}), updateAvatar);
 
 // Получить данные пользователя по Id:
-router.get('/:userId', userIdCelebrate, searchUserById);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().custom(validationId),
+  }),
+}), searchUserById);
 
 module.exports = router;
